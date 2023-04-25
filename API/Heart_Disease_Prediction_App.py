17238905a6a0f2830798bf7436028c15a085f069
HezGee <131678141+HezGee@users.noreply.github.com>
2023-04-25 19:28:26 +0200

    Add files via upload

--- API/Heart_Disease_Prediction_App.py
+++ API/Heart_Disease_Prediction_App.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Heart Disease Prediction App"
+    return "Stroke Disease Prediction App"
 
 @app.route('/predict',methods=['POST'])
 
