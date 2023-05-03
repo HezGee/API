66827a65a709c7e2faf70bcc6f4a5e28bffa13ea
HezGee <131678141+HezGee@users.noreply.github.com>
2023-05-03 18:09:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,92 +17,20 @@ def home():
 #[3,150,0,0,233,145,1,2.3] 1
 
 def predict():
-    def predict():
-        gender = request.form['gender']
-        if gender == 'Male':
-            gender_Male = 1
-            gender_Female = 0
-        else:
-            gender_Male = 0
-            gender_Female = 1
-        age = float(request.form['age'])
-        hypertension = int(request.form['hypertension'])
-        heart_disease = int(request.form['heart_disease'])
-        ever_married = int(request.form['ever_married'])
-        Residence_type = int(request.form['Residence_type'])
-        avg_glucose_level = float(request.form['avg_glucose_level'])
-        bmi = float(request.form['bmi'])
-        work_type = request.form['work_type']
+    gender= request.form.get('gender')
+    age = request.form.get('age')
+    hypertension = request.form.get('hypertension')
+    heart_disease = request.form.get('heart_disease')
+    ever_married = request.form.get('ever_married')
+    work_type = request.form.get('work_type')
+    Residence_type = request.form.get('Residence_type')
+    avg_glucose_level = request.form.get('avg_glucose_level')
+    bmi = request.form.get('bmi')
+    smoking_status = request.form.get('smoking_status')
 
-        if work_type == 'Never_worked':
-            work_type_Never_worked = 1
-            work_type_Private = 0
-            work_type_Self_employed = 0
-            work_type_children = 0
-            work_type_Govt_job = 0
+    #result = {'gender':gender,'age':age,'hypertension':hypertension,'heart_disease':heart_disease,'ever_married':ever_married,'work_type':work_type,'Residence_type':Residence_type,'avg_glucose_level':avg_glucose_level,'bmi':bmi,'smoking_status':smoking_status}
 
-        if work_type == 'Private':
-            work_type_Never_worked = 0
-            work_type_Private = 1
-            work_type_Self_employed = 0
-            work_type_children = 0
-            work_type_Govt_job = 0
-
-        elif work_type == "Self_employed":
-            work_type_Never_worked = 0
-            work_type_Private = 0
-            work_type_Self_employed = 1
-            work_type_children = 0
-            work_type_Govt_job = 0
-
-        elif work_type == "children":
-            work_type_Never_worked = 0
-            work_type_Private = 0
-            work_type_Self_employed = 0
-            work_type_children = 1
-            work_type_Govt_job = 0
-
-        else:
-            work_type_Never_worked = 0
-            work_type_Private = 0
-            work_type_Self_employed = 0
-            work_type_children = 0
-            work_type_Govt_job = 1
-
-
-        smoking_status = request.form['smoking_status']
-
-        if smoking_status == "formerly_smoked":
-            smoking_status_formerly_smoked = 1
-            smoking_status_never_smoked = 0
-            smoking_status_Smokes = 0
-            smoking_status_Unknown = 0
-
-        elif smoking_status == "never_smoked":
-            smoking_status_formerly_smoked = 0
-            smoking_status_never_smoked = 1
-            smoking_status_Smokes = 0
-            smoking_status_Unknown = 0
-
-        elif smoking_status == "Smokes":
-            smoking_status_formerly_smoked = 0
-            smoking_status_never_smoked = 0
-            smoking_status_Smokes = 1
-            smoking_status_Unknown = 0
-
-        else:
-            smoking_status_formerly_smoked = 0
-            smoking_status_never_smoked = 0
-            smoking_status_Smokes = 0
-            smoking_status_Unknown = 1
-
-
-    #result = {'gender':gender,'age':age,'hypertension':hypertension,'heart_disease':heart_disease,'ever_married':ever_married,'Residence_type':Residence_type,'avg_glucose_level':avg_glucose_level,'bmi':bmi,'work_type':work_type,'smoking_status':smoking_status}
-
-    input_query = np.array([[gender_Male,age, hypertension, heart_disease, ever_married,
-                            Residence_type, avg_glucose_level, bmi,
-                            work_type_Never_worked, work_type_Private,work_type_Self_employed, work_type_children,
-                            smoking_status_formerly_smoked, smoking_status_never_smoked, smoking_status_Smokes]])
+    input_query = np.array([[gender,age,hypertension,heart_disease,ever_married,work_type,Residence_type,avg_glucose_level,bmi,smoking_status]])
 
     result = model.predict(input_query)[0]
 
